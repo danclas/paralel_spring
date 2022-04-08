77c1ca3cab65864cbe4e695a751f045206abf5aa
danclas <danvershinin@gmail.com>
2022-04-08 18:04:41 +0300

    Add kernel for getSequentialOperations

--- modules/task_1/vershinin_d_linear_filter_horizontal/linear_filter_horizontal.h
+++ modules/task_1/vershinin_d_linear_filter_horizontal/linear_filter_horizontal.h
@@ -9,6 +9,7 @@ float calcNewPixColor(const int x, const int y, const int width,
                       const int height, const std::vector<float>& kernel,
                       int radius, const std::vector<float>& img);
 std::vector<float> getSequentialOperations(const int width, const int height,
-                                           const std::vector<float> &img);
+                                           const std::vector<float>& kernel,
+                                           const std::vector<float>& img);
 
 #endif  // MODULES_TASK_1_VERSHININ_D_LINEAR_FILTER_HORIZONTAL_LINEAR_FILTER_HORIZONTAL_H_
