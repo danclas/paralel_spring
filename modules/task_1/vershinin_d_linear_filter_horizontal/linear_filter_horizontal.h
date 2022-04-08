27fe79cde149d5c880a3af92ab750d8e7d960321
danclas <danvershinin@gmail.com>
2022-04-08 15:36:44 +0300

    Add necessary function decloration

--- modules/task_1/vershinin_d_linear_filter_horizontal/linear_filter_horizontal.h
+++ modules/task_1/vershinin_d_linear_filter_horizontal/linear_filter_horizontal.h
@@ -2,6 +2,13 @@
 #ifndef MODULES_TASK_1_VERSHININ_D_LINEAR_FILTER_HORIZONTAL_LINEAR_FILTER_HORIZONTAL_H_
 #define MODULES_TASK_1_VERSHININ_D_LINEAR_FILTER_HORIZONTAL_LINEAR_FILTER_HORIZONTAL_H_
 
+#include <vector>
 
+std::vector<float> getGaussKernel(int radius, float sigma);
+float calcNewPixColor(const int x, const int y, const int weight,
+                      const int height, const std::vector<float> &kernel,
+                      const std::vector<float> &img);
+std::vector<float> getSequentialOperations(const int weight, const int height,
+                                           const std::vector<float> &img);
 
 #endif  // MODULES_TASK_1_VERSHININ_D_LINEAR_FILTER_HORIZONTAL_LINEAR_FILTER_HORIZONTAL_H_
